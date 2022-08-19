0c82da3caafc0834e786683c3685865baefbd225
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-19 09:42:04 +0000

    [Update] ユーザ詳細ページ、一覧ページ編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :index, :edit]
+  resources :books, only: [:show, :index, :create, :destroy]
 end
