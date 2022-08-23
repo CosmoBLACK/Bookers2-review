b641110af315e38804ee4db055c6ab08fbd0424f
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-23 15:42:09 +0000

    [Update] コントローラにbefore_action追加、投稿編集画面作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -21,10 +23,30 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:show, :index, :edit, :create]
+  resources :books, only: [:show, :index, :edit, :create, :update]
 end
