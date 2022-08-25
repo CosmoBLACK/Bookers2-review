874e7954d2f89feaecaacdbbf48ac4fc5da2df68
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-25 14:40:50 +0000

    [Add] いいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ApplicationRecord
 
   validates :title, presence:true
   validates :body, presence:true, length:{maximum:200}
+
+  def favorited_by?(user) # このメソッドで、引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べる。
+   favorites.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:show, :index, :edit, :create, :update]
+  resources :books, only: [:show, :index, :edit, :create, :update] do
+    resource :favorites, only: [:create, :destroy] # showページが不要で、idの受け渡しも必要ないのでresourceを使う。ネストすることで、どの投稿(book_id)にいいねを付けたかが特定できる。
+  end
 end
