cfef93d32ac7882f186512b0740e07b8f0f17314
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-29 15:26:59 +0000

    [Update] book_commentsルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:show, :index, :edit, :create, :update] do
+    resources :book_comments, only:[:create, :destroy]
     resource :favorites, only: [:create, :destroy] # showページが不要で、idの受け渡しも必要ないのでresourceを使う。ネストすることで、どの投稿(book_id)にいいねを付けたかが特定できる。
   end
 end
