a2a7ace5a25dce9176a18577863058559d0ce4ca
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-09-02 14:02:45 +0000

    [Update] relationshipルーティング設定、マイグレーションファイル編集

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :books, only: [:show, :index, :edit, :create, :update] do
     resources :book_comments, only:[:create, :destroy]
     resource :favorites, only: [:create, :destroy] # showページが不要で、idの受け渡しも必要ないのでresourceを使う。ネストすることで、どの投稿(book_id)にいいねを付けたかが特定できる。
--- db/migrate/20220902134241_create_relationships.rb
+++ db/migrate/20220902134241_create_relationships.rb
@@ -1,6 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
+      t.integer :follower_id # フォローするユーザのid
+      t.integer :followed_id # フォローされるユーザのid
 
       t.timestamps
     end
